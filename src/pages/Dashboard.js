aa2fc77edf1f3ccfb0e36fadd7c571eda9e0d9da
dinu-dinesh <dineshkumarmca086@gmail.com>
2020-08-24 23:46:37 +0530

    first commit

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,11 +1,25 @@
-import React from 'react';
-import { Info, Repos, User, Search, Navbar } from '../components';
-import loadingImage from '../images/preloader.gif';
-import { GithubContext } from '../context/context';
+import React from "react";
+import { Info, Repos, User, Search, Navbar } from "../components";
+import loadingImage from "../images/preloader.gif";
+import { GithubContext } from "../context/context";
 const Dashboard = () => {
+  const { isLoading } = React.useContext(GithubContext);
+  if (isLoading) {
+    return (
+      <main>
+        <Navbar />
+        <Search />
+        <img src={loadingImage} alt="laoding" className="loading-img" />
+      </main>
+    );
+  }
   return (
     <main>
-      <h2>Dashboard Page</h2>
+      <Navbar></Navbar>
+      <Search></Search>
+      <Info></Info>
+      <User></User>
+      <Repos></Repos>
     </main>
   );
 };
